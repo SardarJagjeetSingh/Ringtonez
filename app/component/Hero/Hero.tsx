72b7fc5b389e3cc475b5caf491a1efd5b20cf486
SardarJagjeetSingh <jj368960@gmail.com>
2024-04-01 20:25:09 +0530

    first commit

---
+++ app/component/Hero/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='h-[174px] w-[1216px] text-[#232323] relative top-[176px] left-[32px] font-[700] text-[67px] Urbanist leading-[87.1px]'>
+            Discover the Perfect <br />
+            Ringtone for Every Mood and Moment
+        </div>
+    )
+}
+
+export default Hero
---
+++ app/component/Navigation/Navigation.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import React from 'react'
+import { FaInstagram, FaYoutube } from 'react-icons/fa'
+
+const Navigation = () => {
+  return (
+    <div className='h-[24px] w-[1216px] relative top-[32px] left-[32px] flex justify-between items-center'>
+      <div className='h-[24px] w-[103px] Urbanist font-[700] text-[16px] leading-[24px]'>Ringtonez</div>
+      <div className='h-[24px] w-[355px] flex justify-between items-center text-[16px] leading-[24px] gap-[48px]'>
+        <Link href={`/`} className='hover:font-[700]'>Home</Link>
+        <Link href={`/`} className='hover:font-[700]'>Ringtonz</Link>
+        <Link href={`/`} className='hover:font-[700]'>About</Link>
+        <Link href={`/`} className='hover:font-[700]'>Contact</Link>
+      </div>
+      <div className='h-[24px] w-[56.88px] gap-[10px] flex items-center'>
+        <FaYoutube/>
+        <FaInstagram/>
+      </div>
+    </div>
+  )
+}
+
+export default Navigation
