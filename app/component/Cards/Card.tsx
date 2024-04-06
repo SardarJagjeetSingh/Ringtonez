a7fde91957845c39e1ee6d187cc0fa9e094dbd3f
SardarJagjeetSingh <jj368960@gmail.com>
2024-04-06 19:33:09 +0530

    shape the card properly but can't work

--- app/component/Cards/Card.tsx
+++ app/component/Cards/Card.tsx
@@ -7,7 +7,7 @@ interface Props{
 
 const Card = ({image}:Props) => {
     return (
-        <div className='h-[320px] w-[389px] rounded-[32px] gap-[8px]'>
+        <div className='h-[320px] w-[389px] rounded-[32px] gap-[8px] overflow-hidden'>
             <Image src={image} alt='img1' layout='fixed' height={500} width={500} objectFit='cover' />
             <div className='h-[127px] w-[389px] overflow-visible p-[32px] bg-[#232323] bg-opacity-[50%] border-t-[4px] border-[#000000] border-opacity-[10%] '></div>
         </div>
